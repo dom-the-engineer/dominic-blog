d6d2bacd90c47ac85ff52d0ec4a9f8bcc01805c2
Dominic Guo <dominicguo317@gmail.com>
2024-07-22 19:50:09 +1000

    Dynamic values in template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the Dominics blog';
+  const likes = 50;
+  const link = "https://dominicguo.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+        <p>{ "hello, lovely people" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+        <a href={ link }>Dominic Site</a>
+
+
       </div>
     </div>
   );
