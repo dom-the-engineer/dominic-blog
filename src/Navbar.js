e1552aa903dd4b5f1a86fa27cf080d9a44946515
Dominic Guo <dominicguo317@gmail.com>
2024-07-23 20:25:28 +1000

    Apply JSX inline styling to Home anchor tag in Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,11 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Dominic Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <a href="/" style={{
+                    color: "white",
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px'
+                }}>Home</a>
                 <a href="/create" style={{
                     color: "white",
                     backgroundColor: '#f1356d',
