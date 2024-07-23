ef5c0bcb9e4120b1a94c48b33670e34933b26ade
Dominic Guo <dominicguo317@gmail.com>
2024-07-23 19:52:38 +1000

    App.js calls Home.js and Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the Dominics blog';
-  const likes = 50;
   const link = "https://dominicguo.com"
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>{ 10 }</p>
-        <p>{ "hello, lovely people" }</p>
-        <p>{ [1,2,3,4,5,6] }</p>
-        <p>{ Math.random() * 10 }</p>
+        <Home />
         <a href={ link }>Dominic Site</a>
 
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,12 @@
 const Navbar = () => {
     return ( 
-        
+        <nav className="navbar">
+            <h1>Dominic Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
      );
 }
  
